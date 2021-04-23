b3ba68530b0bcaae6310b0088595f1da56d1369a
Rilong <romgnatyuk@gmail.com>
2021-04-23 17:28:39 +0300

    Added Condition

--- mwcUploader.js
+++ mwcUploader.js
@@ -1,24 +1,26 @@
 const mwcUploaderButtons = document.querySelectorAll('.mwcUploader__button')
 const mwcUploaderFileInputs = document.querySelectorAll('.mwcUploader__file')
 
-mwcUploaderButtons.forEach(mwcButton => {
-  mwcButton.addEventListener('click', event => {
-    const parent = event.target.closest('.mwcUploader')
-    const fileInput = parent.querySelector('.mwcUploader__file')
-    fileInput.click()
+if(mwcUploaderButtons.length && mwcUploaderFileInputs.length) {
+  mwcUploaderButtons.forEach(mwcButton => {
+    mwcButton.addEventListener('click', event => {
+      const parent = event.target.closest('.mwcUploader')
+      const fileInput = parent.querySelector('.mwcUploader__file')
+      fileInput.click()
+    })
   })
-})
 
-mwcUploaderFileInputs.forEach(fileInput => {
-  fileInput.addEventListener('change', event => {
-    const textField = event.target.closest('.mwcUploader').querySelector('.mwcUploader__text')
-    const unprocessedPath = event.target.value
-    let proccessedPath = ''
-    let slash = unprocessedPath.indexOf('\/') !== -1 ? '/' : '\\'
+  mwcUploaderFileInputs.forEach(fileInput => {
+    fileInput.addEventListener('change', event => {
+      const textField = event.target.closest('.mwcUploader').querySelector('.mwcUploader__text')
+      const unprocessedPath = event.target.value
+      let proccessedPath = ''
+      let slash = unprocessedPath.indexOf('\/') !== -1 ? '/' : '\\'
 
-    proccessedPath = unprocessedPath.split(slash)
-    proccessedPath = proccessedPath[proccessedPath.length - 1]
-    
-    textField.value = proccessedPath
+      proccessedPath = unprocessedPath.split(slash)
+      proccessedPath = proccessedPath[proccessedPath.length - 1]
+      
+      textField.value = proccessedPath
+    })
   })
-})
+}
